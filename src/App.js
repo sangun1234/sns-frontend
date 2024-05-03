618caa66d1062a45c76d11c1cc5e77c73f0b7239
sangun1234 <sangun5104@gmail.com>
2024-05-03 14:46:17 +0900

    reacitve variable

--- src/App.js
+++ src/App.js
@@ -4,37 +4,33 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 import { useState } from "react";
-//라우터 선언.
-// 브라우저라우터 : html5의 히스토리 api사용
-//해쉬라우터: url 사용
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "./apollo";
+
+//오늘 공부할 주제 : reactive variables
+/* 
+  아폴로 클라이언트에도 포함되어 있어.
+  이게뭐냐 ? reactive variable는 기본적으로 반응하고 변하는 베리어블이다.
+  예를 들어서 sns에서는 다크모드 / 로그인 여부 등이 있다.
+
+  이런 리엑티브 베리어블을 리엑트 컴포넌트 내에서 사용하고 싶다면,
+  우리는 hook을 사용해야한다.
+
+*/
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // useReactiveVar으로 isLoggedInVar이라는 reactive variable가져오기 .(훅)
+  // 우리가 원하는 어느 컴포넌트에서도 isLoggedInVar을 바꿀 수 있다.
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
     <div>
       <Router>
         <Switch>
-          {/* 스위치는 한번에 딱 하나의 route만 랜더링 시킴.
-          얘를 안쓰면 route가 다 중복되서 보여진다.
-          하지만 이럴경우 path가 / 인녀석만 출력이 되기때문에
-          exact를 써줘야된다. url이 정확히 맞는지 확인하는 것.
-          */}
           <Route path="/" exact>
-            {isLoggedIn ? (
-              <Home setIsLoggedIn={setIsLoggedIn} />
-            ) : (
-              // 이거처럼 prop을 계속 아래로 보내는 행위는 좋지 않다고 한다.
-              //따라서 apolloClient로 로컬 state를 가져오도록 할거임.
-              <Login setIsLoggedIn={setIsLoggedIn} />
-            )}
+            {isLoggedIn ? <Home /> : <Login />}
           </Route>
           <Route>
             <NotFound />
-            {/* 뭔 이상한거를 url에써서 들어올라하면 오류라고 알려주는 문구 */}
-            {/* 맨 마지막에 와야되는 것이 중요하다. */}
-
-            {/* <Redirect to="/" /> */}
-            {/* 이런 방식으로도 사용 가능하다. 리다이렉트로 홈으로 보내는 것이다. */}
           </Route>
         </Switch>
       </Router>
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,14 @@
-const Login = ({ setIsLoggedIn }) => (
+import { isLoggedInVar } from "../apollo";
+
+const Login = () => (
   <div>
     <h1>Login</h1>
-    <button onClick={() => setIsLoggedIn(true)}>Log in now</button>
+
+    {/* 이런식으로 reactive variable을 선언해놓은 apollo.js를 그대로 가져와서
+        바꿔버리면 된다. 이렇게 하면 prop을 사용하지 않고도
+        유저의 로그인 여부를 결정할 수 있게 된다.
+    */}
+    <button onClick={() => isLoggedInVar(true)}>Log in now</button>
   </div>
 );
 export default Login;
